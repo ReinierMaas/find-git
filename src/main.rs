3bbb014d37ef922c1478dbb31251a4b5411a14a8
Reinier Maas <reinier.maas@channable.com>
2022-06-27 19:57:55 +0200

    Run: `cargo fmt`

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use colored::Colorize;
+use git2::{BranchType, Repository, Status};
 use std::env;
 use std::path::PathBuf;
-use std::{io,io::{Error, ErrorKind}};
+use std::{
+    io,
+    io::{Error, ErrorKind},
+};
 use walkdir::{DirEntry, WalkDir};
-use git2::{Repository, Status, BranchType};
-use colored::Colorize;
 
 // Get git status
 fn git_status() -> Status {
@@ -19,52 +22,58 @@ fn from_git_error(e: git2::Error) -> Error {
 
 // DirEntry is a '.git' folder
 fn is_git(entry: &DirEntry) -> bool {
-    entry.file_name()
-         .to_str()
-         .map(|s| s == ".git")
-         .unwrap_or(false)
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s == ".git")
+        .unwrap_or(false)
 }
 
 fn main() -> io::Result<()> {
     let cmp_status = git_status();
     let current_dir = env::current_dir()?;
     let walker = WalkDir::new(current_dir).into_iter();
-    let mut repos : Vec<PathBuf> = vec![];
+    let mut repos: Vec<PathBuf> = vec![];
 
-    println!("{}","Found repositories:".bold().blue());
+    println!("{}", "Found repositories:".bold().blue());
     for entry in walker.filter_map(|e| e.ok()) {
         if let Some(parent) = entry.path().parent() {
             if is_git(&entry) {
                 let parent_display = format!("    {}", parent.display());
-                println!("{}",parent_display.italic().green());
+                println!("{}", parent_display.italic().green());
                 repos.push(parent.to_owned());
             }
         }
     }
 
-    println!("{}","Repositories with uncommitted changes:".bold().blue());
+    println!("{}", "Repositories with uncommitted changes:".bold().blue());
     for parent in &repos {
         let repo = Repository::open(&parent).map_err(from_git_error)?;
         let statuses = repo.statuses(None).map_err(from_git_error)?;
         if statuses
             .iter()
-            .any(|status| status.status().intersects(cmp_status)) {
+            .any(|status| status.status().intersects(cmp_status))
+        {
             let parent = format!("    {}", parent.display());
             println!("{}", parent.italic().green());
-            for status in statuses.iter().filter(|s| s.path().is_some() && s.status().intersects(cmp_status)) {
+            for status in statuses
+                .iter()
+                .filter(|s| s.path().is_some() && s.status().intersects(cmp_status))
+            {
                 println!("        {}", status.path().unwrap().cyan());
             }
         }
     }
 
-    println!("{}","Repositories with stashed changes:".bold().blue());
+    println!("{}", "Repositories with stashed changes:".bold().blue());
     for parent in &repos {
         let mut repo = Repository::open(&parent).map_err(from_git_error)?;
         let mut stashes = vec![];
         repo.stash_foreach(|_, stash, _| {
             stashes.push(stash.to_owned());
             true
-        }).map_err(from_git_error)?;
+        })
+        .map_err(from_git_error)?;
         if !stashes.is_empty() {
             let parent = format!("    {}", parent.display());
             println!("{}", parent.italic().green());
@@ -74,22 +83,40 @@ fn main() -> io::Result<()> {
         }
     }
 
-    println!("{}","Repositories with unpushed branches:".bold().blue());
+    println!("{}", "Repositories with unpushed branches:".bold().blue());
     for parent in &repos {
         let repo = Repository::open(&parent).map_err(from_git_error)?;
-        let branches = repo.branches(Some(BranchType::Local)).map_err(from_git_error)?;
+        let branches = repo
+            .branches(Some(BranchType::Local))
+            .map_err(from_git_error)?;
         let diff_branches: Vec<String> = branches
-            .filter_map(|r|r.ok())
-            .filter(|(branch,_)| {
+            .filter_map(|r| r.ok())
+            .filter(|(branch, _)| {
                 if let Ok(upstream) = branch.upstream() {
-                    let upstream = upstream.get().peel_to_tree().map_err(from_git_error).unwrap();
+                    let upstream = upstream
+                        .get()
+                        .peel_to_tree()
+                        .map_err(from_git_error)
+                        .unwrap();
                     let branch = branch.get().peel_to_tree().map_err(from_git_error).unwrap();
-                    let diff = repo.diff_tree_to_tree(Some(&upstream), Some(&branch), None).map_err(from_git_error).unwrap();
+                    let diff = repo
+                        .diff_tree_to_tree(Some(&upstream), Some(&branch), None)
+                        .map_err(from_git_error)
+                        .unwrap();
                     diff.deltas().any(|_| true)
                 } else {
                     true
-                }})
-            .map(|(branch,_)| branch.name().map_err(from_git_error).unwrap().unwrap_or("").to_owned()).collect();
+                }
+            })
+            .map(|(branch, _)| {
+                branch
+                    .name()
+                    .map_err(from_git_error)
+                    .unwrap()
+                    .unwrap_or("")
+                    .to_owned()
+            })
+            .collect();
         if !diff_branches.is_empty() {
             let parent = format!("    {}", parent.display());
             println!("{}", parent.italic().green());
